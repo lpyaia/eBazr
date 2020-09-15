401dd7e57ee19d25d9993d034f1e4bf0be01c231
Lucas Pyaia França <brhbs0551@ambev.com.br>
2020-09-14 23:42:48 -0300

    mongodb connection config

--- containers/Catalog.Api/Startup.cs
+++ containers/Catalog.Api/Startup.cs
@@ -26,6 +26,8 @@ namespace Catalog.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            BootStrapper.RegisterServices(services, Configuration);
+
             services.AddControllers();
 
             #region Configuration Dependencies
